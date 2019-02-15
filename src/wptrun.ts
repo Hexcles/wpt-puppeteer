7e97e8297d794ae4e0663c8ef880b0109d9d22b1
Robert Ma <robertma@chromium.org>
2019-02-14 19:54:25 -0500

    Refactor wptrun into a class

--- src/wptrun.ts
+++ src/wptrun.ts
@@ -28,175 +28,182 @@ const BROWSER_ARGS = [
   "--ignore-certificate-errors",
 ];
 
-function getCurrentPages(browser: puppeteer.Browser): Promise<puppeteer.Page[]> {
-  return new Promise((resolve) => {
-    browser.pages().then(( pages ) => resolve(pages))
-    .catch(() => {
-      // Most likely the test page just closed a page. Retry.
-      setTimeout(() => {
-        resolve(browser.pages());
-      }, 1000);
+class Runner {
+  public static async new(headless: boolean): Promise<Runner> {
+    return new Runner(headless, await Runner.launchBrowser(headless));
+  }
+
+  private static launchBrowser(
+    headless: boolean,
+    viewport: puppeteer.Viewport = {width: 800, height: 600},
+  ): Promise<puppeteer.Browser> {
+    // tslint:disable: object-literal-sort-keys
+    return puppeteer.launch({
+      // executablePath: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+      args: BROWSER_ARGS,
+      headless,
+      ignoreHTTPSErrors: true,
+      // This only resizes the viewport, not the window.
+      defaultViewport: viewport,
     });
-  });
-}
-
-async function getNewPage(browser: puppeteer.Browser) {
-  const oldPages = await getCurrentPages(browser);
-  // Keep the default blank page to prevent the browser from exiting.
-  await Promise.all(oldPages.slice(1).map((p) => p.close()));
-  // Create another new page in case the page closes itself.
-  return await browser.newPage();
-}
-
-async function runSingleTest(
-  browser: puppeteer.Browser,
-  executor: typeof Executor,
-  test: string,
-  info: ManifestInfo): Promise<Result> {
-  // TODO: verify the timeout & apply timeout multipler.
-  const externalTimeout = EXT_TIMEOUT_MULTIPLIER * (HARNESS_TIMEOUT[info.timeout || "normal"]);
-  const url = getTestURL(test);
-
-  const page = await getNewPage(browser);
-  const controller = new executor(page, test, externalTimeout);
-  await controller.installBindings();
-  if (info.testdriver) {
-    await controller.installTestDriverBindings();
   }
 
-  return controller.runTest(url);
-}
+  constructor(
+    private headless: boolean,
+    private browser: puppeteer.Browser,
+  ) {}
 
-function shouldRun(test: string, testPrefixes: string[]): boolean {
-  for (const testPrefix of testPrefixes) {
-    if (test.startsWith(testPrefix)) {
-      return true;
+  public async run(wptDir: string, wptReport: string, testPrefixes: string[]) {
+    if (testPrefixes.length === 0) {
+      testPrefixes.push("");
     }
-  }
-  return false;
-}
 
-function getTestURL(test: string): string {
-  if (test === "about:blank") {
-    return test;
-  }
-  const useHTTPS = test.includes(".https.") || test.includes(".serviceworker.");
-  // These are the default port numbers.
-  return  useHTTPS ?
-    `https://web-platform.test:8443${test}` :
-    `http://web-platform.test:8000${test}`;
-}
-
-async function run(args: {[key: string]: any}) {
-  const wptDir = args.wpt_dir as string;
-  const wptReport = args.log_wptreport as string;
-  const headless = args.headless as boolean;
-  const testPrefixes = args.TEST as string[];
-
-  if (testPrefixes.length === 0) {
-    testPrefixes.push("");
-  }
-
-  for (let i = 0; i < testPrefixes.length; i++) {
-    if (!testPrefixes[i].startsWith("/")) {
-      testPrefixes[i] = "/" + testPrefixes[i];
-    }
-  }
-
-  const manifestReader = new ManifestReader(path.join(wptDir, "MANIFEST.json"));
-  const manifest = manifestReader.manifest;
-
-  // tslint:disable: object-literal-sort-keys
-  let browser = await puppeteer.launch({
-    // executablePath: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
-    args: BROWSER_ARGS,
-    headless,
-    ignoreHTTPSErrors: true,
-    // This only resizes the viewport, not the window.
-    defaultViewport: {
-        width: 800,
-        height: 600,
-    },
-  });
-  // tslint:enable: object-literal-sort-keys
-
-  const results: Result[] = [];
-
-  for (const [file, tests] of Object.entries(manifest.items.testharness)) {
-    for (const [test, info] of tests) {
-      if (info.jsshell || !shouldRun(test, testPrefixes)) {
-        continue;
+    for (let i = 0; i < testPrefixes.length; i++) {
+      if (!testPrefixes[i].startsWith("/")) {
+        testPrefixes[i] = "/" + testPrefixes[i];
       }
+    }
 
-      const result: Result = await runSingleTest(browser, TestharnessExecutor, test, info);
-      logger.debug(result);
-      results.push(result);
+    const manifestReader = new ManifestReader(path.join(wptDir, "MANIFEST.json"));
+    const manifest = manifestReader.manifest;
 
-      logger.log(result.toString());
-      if (result.subtests) {
-        for (const subtest of result.subtests) {
-          logger.log("  " + subtest.toString());
+    const results: Result[] = [];
+
+    for (const [file, tests] of Object.entries(manifest.items.testharness)) {
+      for (const [test, info] of tests) {
+        if (info.jsshell || !this.shouldRun(test, testPrefixes)) {
+          continue;
+        }
+
+        const result: Result = await this.runSingleTest(TestharnessExecutor, test, info);
+        logger.debug(result);
+        results.push(result);
+
+        logger.log(result.toString());
+        if (result.subtests) {
+          for (const subtest of result.subtests) {
+            logger.log("  " + subtest.toString());
+          }
         }
       }
     }
-  }
 
-  await browser.close();
-
-  // tslint:disable: object-literal-sort-keys
-  browser = await puppeteer.launch({
-    args: BROWSER_ARGS,
-    headless,
-    ignoreHTTPSErrors: true,
     // Reftests use a different viewport size.
-    defaultViewport: {
-        width: 600,
-        height: 600,
-    },
-  });
-  // tslint:enable: object-literal-sort-keys
+    await this.restartBrowser({width: 600, height: 600});
 
-  for (const [file, tests] of Object.entries(manifest.items.reftest)) {
-    for (const [test, references, info] of tests) {
-      if (!shouldRun(test, testPrefixes) || references.length === 0) {
-        continue;
-      }
+    for (const [file, tests] of Object.entries(manifest.items.reftest)) {
+      for (const [test, references, info] of tests) {
+        if (!this.shouldRun(test, testPrefixes) || references.length === 0) {
+          continue;
+        }
 
-      const testResult: Result = await runSingleTest(browser, RefTestExecutor, test, info);
-      logger.debug(testResult);
+        const testResult: Result = await this.runSingleTest(RefTestExecutor, test, info);
+        logger.debug(testResult);
 
-      if (testResult.status === TestsStatus.OK) {
-        testResult.status = TestStatus.FAIL;
-        // TODO: Support reference chain.
-        for (const [ref, condition] of references) {
-          const refResult: Result = await runSingleTest(browser, RefTestExecutor, ref, info);
-          logger.debug(refResult);
+        if (testResult.status === TestsStatus.OK) {
+          testResult.status = TestStatus.FAIL;
+          // TODO: Support reference chain.
+          for (const [ref, condition] of references) {
+            const refResult: Result = await this.runSingleTest(RefTestExecutor, ref, info);
+            logger.debug(refResult);
 
-          if (refResult.status === TestsStatus.TIMEOUT) {
-            testResult.status = TestsStatus.TIMEOUT;
-            testResult.message = "ref timeout";
-            break;
-          }
+            if (refResult.status === TestsStatus.TIMEOUT) {
+              testResult.status = TestsStatus.TIMEOUT;
+              testResult.message = "ref timeout";
+              break;
+            }
 
-          const passed = (condition === "==") === (refResult.hashScreen() === testResult.hashScreen());
-          if (passed) {
-            testResult.status = TestStatus.PASS;
-            break;
+            const passed = (condition === "==") === (refResult.hashScreen() === testResult.hashScreen());
+            if (passed) {
+              testResult.status = TestStatus.PASS;
+              break;
+            }
           }
         }
-      }
 
-      results.push(testResult);
-      logger.log(testResult.toString());
+        results.push(testResult);
+        logger.log(testResult.toString());
+      }
     }
+
+    await this.browser.close();
+
+    fs.writeFileSync(wptReport, JSON.stringify({results}) + "\n");
   }
 
-  await browser.close();
+  private async restartBrowser(viewport?: puppeteer.Viewport) {
+    logger.debug("Restarting browser...")
+    await this.browser.close();
+    this.browser = await Runner.launchBrowser(this.headless, viewport);
+    logger.debug("Browser restarted.")
+  }
 
-  fs.writeFileSync(wptReport, JSON.stringify({results}) + "\n");
+  private getCurrentPages(): Promise<puppeteer.Page[]> {
+    return new Promise((resolve) => {
+      this.browser.pages().then(( pages ) => resolve(pages))
+      .catch(() => {
+        // Most likely the test page just closed a page. Retry.
+        setTimeout(() => {resolve(this.browser.pages()); }, 1000);
+      });
+    });
+  }
+
+  private async getNewPage(): Promise<puppeteer.Page> {
+    const oldPages = await this.getCurrentPages();
+    // Keep the default blank page to prevent the browser from exiting.
+    const promises = [
+      Promise.all(oldPages.slice(1).map((p) => p.close())),
+      new Promise((resolve) => {setTimeout(resolve, 1000, "timeout"); }),
+    ];
+    const result = await Promise.race(promises);
+    if (result === "timeout") {
+      await this.restartBrowser();
+    }
+    // Create another new page in case the page closes itself.
+    return await this.browser.newPage();
+  }
+
+  private async runSingleTest(
+    executor: typeof Executor,
+    test: string,
+    info: ManifestInfo): Promise<Result> {
+    // TODO: verify the timeout & apply timeout multipler.
+    const externalTimeout = EXT_TIMEOUT_MULTIPLIER * (HARNESS_TIMEOUT[info.timeout || "normal"]);
+    const url = this.getTestURL(test);
+
+    const page = await this.getNewPage();
+    const controller = new executor(page, test, externalTimeout);
+    await controller.installBindings();
+    if (info.testdriver) {
+      await controller.installTestDriverBindings();
+    }
+
+    logger.debug("Starting: " + url);
+    return controller.runTest(url);
+  }
+
+  private shouldRun(test: string, testPrefixes: string[]): boolean {
+    for (const testPrefix of testPrefixes) {
+      if (test.startsWith(testPrefix)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private getTestURL(test: string): string {
+    if (test === "about:blank") {
+      return test;
+    }
+    const useHTTPS = test.includes(".https.") || test.includes(".serviceworker.");
+    // These are the default port numbers.
+    return useHTTPS ?
+      `https://web-platform.test:8443${test}` :
+      `http://web-platform.test:8000${test}`;
+  }
 }
 
-function main() {
+async function main() {
   const parser = new ArgumentParser({
     addHelp: true,
     description: "Run WPT on Chrome using puppeteer.",
@@ -219,7 +226,8 @@ function main() {
     nargs: "*",
   });
   const args = parser.parseArgs();
-  run(args);
+  const runner = await Runner.new(args.headless);
+  runner.run(args.wpt_dir, args.log_wptreport, args.TEST);
 }
 
 main();
